a690e1b92b23d9dcbc4916d4c37b4bdb7f23d561
nico-slk <nico1991.6@gmail.com>
2023-09-18 19:12:46 -0300

    fix code

--- clase 4/codeWar.js
+++ clase 4/codeWar.js
@@ -18,8 +18,7 @@ function positiveSum(arr) {
 // Complete the function to return his total number of goals in all three leagues.
 //   Note: the input will always be valid.
 // For example:
-
-5, 10, 2  -- > 17;
+// 5, 10, 2  -- > 17;
 
 function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
